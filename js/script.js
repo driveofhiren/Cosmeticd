8ae26e87470719c77918738729436267cec151e0
Hiren Gohil <50377062+driveofhiren@users.noreply.github.com>
2024-01-21 16:21:21 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function validateForm() {
+  var name = document.forms["myForm"]["name"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var phone = document.forms["myForm"]["phone"].value;
+  var message = document.forms["myForm"]["message"].value;
+
+  if (name == "") {
+    alert("Field Required");
+    return false;
+  }
+
+  if (email == "") {
+    alert("Field Required");
+    return false;
+  }
+  if (phone == "") {
+    alert("Field Required");
+    return false;
+  }
+
+  if (message == "") {
+    alert("Field Required");
+    return false;
+  }
+
+  return true;
+}
+
+
+
